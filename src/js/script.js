046516112efa9abc7e4f4c30138818f1ffc53362
Aleksey Renner <490748578@mail.ru>
2021-03-26 20:55:41 +0300

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prevArrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/nextArrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            },
+        ]
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+});
